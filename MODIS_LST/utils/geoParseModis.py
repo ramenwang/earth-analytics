7780f8d196ba775c638c5733d2293f3cab968f39
ramenwang <ramenwangqing@gmail.com>
2019-12-07 16:26:18 -0600

    add functions and fix bugs

--- MODIS_LST/utils/geoParseModis.py
+++ MODIS_LST/utils/geoParseModis.py
@@ -4,6 +4,7 @@ from pyhdf.SD import SD, SDC
 from utils import parsemodis
 from utils.maskmodis import ModisQuality, Masker
 import matplotlib.pyplot as plt
+import matplotlib.colors as colors
 import earthpy.plot as ep
 import numpy as np
 
@@ -82,9 +83,97 @@ class getModisLST_1km():
         a function to plot LST data
         '''
         plt.figure(figsize=(20,10))
-        im = plt.imshow(self.data)
-        ep.colorbar(im)
+        im = plt.imshow(self.toCelsius(self.data))
+        im_bar = plt.colorbar(im, shrink = 0.75)
+        im_bar.set_label('LST (Celsius)')
         plt.show()
+
+
+    def plotOverlap(self, zval=None, z_label=None):
+        '''
+        a function to plot LST data with spatial points on top
+        :param zval (numeric array): specifies the z-value to be plot
+                                     long with the spatial points
+        '''
+        plt.figure(figsize=(20,10))
+
+        # set up image plot
+        im = plt.imshow(self.toCelsius(self.data))
+        im_bar = plt.colorbar(im, shrink = 0.75)
+        # im_bar = ep.colorbar(im)
+        # set up labels
+        plt.xlabel('Columns')
+        plt.ylabel('Rows')
+        im_bar.set_label('LST (Celsius)')
+
+        # set up points plot
+        _, _, rows, cols = self.toMatrixCoor()
+        if zval != None:
+            zval = np.array(zval)
+            points = plt.scatter(x=cols, y=rows, c=zval,
+                                 norm=colors.Normalize(vmin=zval.min(), vmax=zval.max()),
+                                 cmap='coolwarm')
+            pts_bar = plt.colorbar(points, shrink = 0.75)
+            if z_label != None:
+                pts_bar.set_label(z_label)
+        else:
+            plt.scatter(x=cols, y=rows, c='magenta')
+        # show
+        plt.show()
+
+
+    def plotPoints(self, zval=None, z_label=None):
+        '''
+        a function to plot LST data with spatial points on top
+        :param zval (numeric array): specifies the z-value to be plot
+                                     long with the spatial points
+        '''
+        plt.figure(figsize=(20,10))
+        plt.gca().invert_yaxis()
+
+        # set up points plot
+        _, _, rows, cols = self.toMatrixCoor()
+        if zval != None:
+            zval = np.array(zval)
+            points = plt.scatter(x=cols, y=rows, c=zval,
+                                 norm=colors.Normalize(vmin=zval.min(), vmax=zval.max()),
+                                 cmap='coolwarm')
+            pts_bar = plt.colorbar(points, shrink = 0.25)
+            if z_label != None:
+                pts_bar.set_label(z_label)
+        else:
+            plt.scatter(x=cols, y=rows)
+        # show
+        plt.show()
+
+
+    def toMatrixCoor(self):
+        '''
+        a function to convert spatial coordinates to matrix coordinates
+        '''
+
+        # get geoinformation
+        bound = self.modisParser.retBoundary()
+        long_res = (bound['max_lon'] - bound['min_lon']) / self.data.shape[1] # col - x
+        lat_res = (bound['max_lat'] - bound['min_lat']) / self.data.shape[0] # row - y
+
+        # retreive value based on lat/long
+        rows = ((bound['max_lat'] - self.lats) / lat_res).astype(int)
+        cols = ((self.longs - bound['min_lon']) / long_res).astype(int)
+
+        # trim those locations out of bounds
+        row_outlier = (rows<0) | (rows >= self.data.shape[0])
+        col_outlier = (cols<0) | (cols >= self.data.shape[1])
+        outliers = row_outlier | col_outlier
+        print(f'There are {sum(outliers)} has been excluded due to laying out boundary')
+
+        # updates location parameters
+        rows = rows[(outliers == False)]
+        cols = cols[(outliers == False)]
+        lats = self.lats[(outliers == False)]
+        longs = self.longs[(outliers == False)]
+
+        return lats, longs, rows, cols
 
 
     def toCelsius(self, modis_data):
@@ -114,26 +203,7 @@ class getModisLST_1km():
         elif dataType == 'qc_mask':
             modis_data = self.mask
 
-        # get geoinformation
-        bound = self.modisParser.retBoundary()
-        long_res = (bound['max_lon'] - bound['min_lon']) / self.data.shape[1] # col - x
-        lat_res = (bound['max_lat'] - bound['min_lat']) / self.data.shape[0] # row - y
-
-        # retreive value based on lat/long
-        rows = ((bound['max_lat'] - self.lats) / lat_res).astype(int)
-        cols = ((self.longs - bound['min_lon']) / long_res).astype(int)
-
-        # trim those locations out of bounds
-        row_outlier = (rows<0) | (rows >= self.data.shape[0])
-        col_outlier = (cols<0) | (cols >= self.data.shape[1])
-        outliers = row_outlier | col_outlier
-        print(f'There are {sum(outliers)} has been excluded due to laying out boundary')
-
-        # updates location parameters
-        rows = rows[(outliers == False)]
-        cols = cols[(outliers == False)]
-        lats = self.lats[(outliers == False)]
-        longs = self.longs[(outliers == False)]
+        lats, longs, rows, cols = self.toMatrixCoor()
 
         # return value from modis_data
         return np.array(list(zip(longs, lats, [modis_data[(row, col)] for row, col in zip(rows, cols)])))
