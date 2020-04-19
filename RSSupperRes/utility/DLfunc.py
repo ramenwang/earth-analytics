f7c054f29544e49f7de94bdef50b4ed5425f06e1
Qing Wang <raymondwang@Qings-MBP.attlocal.net>
2020-04-19 00:22:31 -0500

    add function files

---
+++ RSSupperRes/utility/DLfunc.py
@@ -0,0 +1,186 @@
+import numpy as np
+import tensorflow as tf
+import keras as K
+from keras.layers import Input, Concatenate, Conv2D, LeakyReLU, Dense, Lambda
+from keras.layers import BatchNormalization, Dropout
+from keras.optimizers import Adam
+from keras.models import Model
+
+
+def conv2d(x, num_filters, filter_size, stride=(1,1), padding='same', name="",
+           act=True, batchnorm=True, dropout_rate=None):
+    ''' conv2d with optional batchnorm, leakyReLU activation, or dropout
+    :param x: input tensor
+    :param num_filters: number of filters
+    :param filter_size: filter size
+    :param stride: convolution stride
+    :param padding: padding
+    :param name: give name to the convolutional operation
+    :param act: if giving activation
+    :param batchnorm: if implement batch normalization
+    :param dropout_rate: if None no random dropout, otherwise random dropout based on the rate
+    :return: a tensor after convolutional operation
+    :rtype: tensor
+    '''
+    x = Conv2D(filters=num_filters, kernel_size=filter_size, strides=stride,
+               padding=padding, name=name+'_conv2d')(x)
+    # batch normalization
+    if (batchnorm):
+        x = BatchNormalization()(x)
+    # activation
+    if (act):
+        x = LeakyReLU()(x)
+    # random dropout
+    if (dropout_rate != None):
+        x = Dropout(rate=dropout_rate)(x)
+
+    return x
+
+
+def inception_module(x, nin_filter, dropout_rate,batchnorm=True, name=""):
+    ''' inception layer with bottleneck layer to reduce the depth
+    :param nin_filter1: number of filters used on inception path 1
+    :param nin_filter2: number of filters used on inception path 2
+    :param dropout_rate: if None no random dropout, otherwise random dropout based on the rate
+    :param name: give name to the convolutional operation
+    :param batchnorm: if implement batch normalization
+    :return: tensor after inceptional feature selection
+    :rtype: tensor
+    '''
+    # path 1 - bottleneck layer
+    p1 = conv2d(x, num_filters=nin_filter, filter_size=(1,1), name=name+'_p1_bn',
+                act=True, batchnorm=batchnorm, dropout_rate=dropout_rate)
+    # path 2 - bottleneck layer + 3*3 filter
+    nin_filter2 = nin_filter // 2
+    p2 = conv2d(x, num_filters=nin_filter2, filter_size=(1,1), name=name+'_p2_bn',
+                act=True, batchnorm=batchnorm, dropout_rate=dropout_rate)
+    p2 = conv2d(p2, num_filters=nin_filter2, filter_size=(3,3), name=name+'_p2',
+                act=True, batchnorm=batchnorm, dropout_rate=dropout_rate)
+    # path 3 - bottleneck layer + 3*3 filter
+    p3 = conv2d(x, num_filters=nin_filter2, filter_size=(1,1), name=name+'_p3_bn',
+                act=True, batchnorm=batchnorm, dropout_rate=dropout_rate)
+    p3 = conv2d(p3, num_filters=nin_filter2, filter_size=(5,5), name=name+'_p3',
+                act=True, batchnorm=batchnorm, dropout_rate=dropout_rate)
+
+    output = K.layers.concatenate([p1, p2, p3], axis=3, name=name+'_concat')
+    return output
+
+
+def pixel_shuffler(x, scale, output_filters, filter_size):
+    ''' pixel shuffler layer to reconstruct the 2d space from feature map channels
+    :param x: tensor object
+    :param scale: scale factor to target image spatial resolution
+    :param output_filters: number of the channels output from shuffler
+    :param filter_size: the size of filter
+    :return: a tensor which has the target spatial resolution and channels
+    :rtype: activated tensor
+    '''
+    ps = conv2d(x, num_filters=output_filters*scale*scale, filter_size=filter_size,
+                name='ps', batchnorm=False, act=False, dropout_rate=None)
+    # using depth_to_space to resampling the image pixel from depth
+    Subpixel_layer = Lambda(lambda x: tf.nn.depth_to_space(x,scale))
+    ps = Subpixel_layer(inputs=ps)
+    ps = LeakyReLU()(ps)
+    return ps
+
+
+def feature_extractor(x, n_layers, filter_size, n_filters, min_filters,
+                      filter_decay_gamma, dropout_rate):
+    ''' building feature extractor with skip connection and filter gamma decay
+    :param x: input volumn
+    :param n_layers: number of convolutional layers for feature extraction
+    :param filter_size: size of the convolution filters
+    :param n_filters: number of filters at the first layer
+    :param min_filter: the minimum number of filters, normally the deepest layer
+    :param filter_decay_gamma: the hyperparameter controls the number of filters
+    :param dropout_rate: the rate of random dropout
+    :return: a stack of skip connected tensors
+    :rtype: tensors
+    '''
+    concat_list = []
+    n_filter_list = []
+    for i in range(n_layers):
+        # perform gamma decay
+        if min_filters !=0 and i > 0:
+            x1 = i / float(n_layers - 1)
+            y1 = pow(x1, 1. / filter_decay_gamma)
+            n_filters = int((n_filters - min_filters) * (1 - y1) + min_filters)
+            n_filter_list.append(n_filters)
+        # perform convolution
+        x = conv2d(x, num_filters=n_filters, filter_size=filter_size, name='FE_'+str(i),
+                   act=True, batchnorm=True, dropout_rate=dropout_rate)
+        concat_list.append(x)
+
+    output = K.layers.concatenate(concat_list, axis=3, name='FE_concat')
+    return output, n_filter_list
+
+
+def get_psnr_tensor(x, mse):
+    ''' compute PSNR for from MSE
+    :param x: input tensor
+    :param mse: mean square error
+    :retun: PSNR value
+    :rtype: mes.dtype
+    '''
+    value = tf.constant(tf.math.reduce_max(x), dtype=mse.dtype) / tf.sqrt(mse)
+    numerator = tf.log(value)
+    denominator = tf.log(tf.constant(10, dtype=mse.dtype))
+    return tf.constant(20, dtype=mse.dtype) * numerator / denominator
+
+
+def img_loss(border_size):
+    ''' mean square error for tensor
+    :param img_true: true image
+    :param img_pred: predicted rescaled image
+    :param border_size: according to the study, it is set to 2*filter_size+scale
+    :return: mean sqaure error
+    :rtype: constant
+    '''
+    def loss(img_true, img_pred):
+        diff = K.layers.subtract([img_true, img_pred])
+        if border_size>0:
+            # trim image into wanted border_size
+            diff = diff[border_size: -border_size, border_size: -border_size]
+        return K.backend.mean(K.backend.square(diff), axis=-1)
+
+    return loss
+
+
+def RSSuperRes(input_dim, scale, n_layers, n_filters, min_filters,
+               filter_decay_gamma, dropout_rate, nin_filter,
+               shuffler_output_filters, shuffler_filter_size,
+               n_reconstructors, num_recon_filters, recon_filter_size,
+               output_channels):
+    ''' residual super resolution model, which includes four parts - feature extractor,
+    inception feature selector, resolution resampler.
+    :param x: input residual image between bicubic resampled image and target
+    :param bicubic_x: input bicubic resampled image
+    :param n_layers: number of layers in the feature extractor
+    :param n_filters: number of filters in the first layer in feature extractor
+    :param min_filters: number of filters in the deepest layer in feature extractor
+    :param filter_decay_gamma: the hyperparameter controls the number of filters
+    :param dropout_rate: the rate of random dropout
+    :param nin_filter: number of filters used in inception module
+    :param n_reconstructors: number of convolutional layers for resampling target
+    :param num_recon_filters: number of filters in reconstructor
+    :param output_channels: number of channels in target image
+    :return: target image with higher resolution
+    :rtype: tensor
+    '''
+    inputs = Input(shape=input_dim)
+    x, n_filter_list = feature_extractor(inputs, n_layers, (3,3), n_filters,
+                                         min_filters, filter_decay_gamma, dropout_rate)
+    print("number of filters used in different layers are:")
+    print(n_filter_list)
+    x = inception_module(x=x, nin_filter=nin_filter, dropout_rate=dropout_rate, name='inc')
+    x = pixel_shuffler(x=x, scale=scale, output_filters=shuffler_output_filters,
+                       filter_size=shuffler_filter_size)
+    for i in range(max(1, n_reconstructors)):
+        x = conv2d(x=x, num_filters=num_recon_filters, filter_size=recon_filter_size,
+                   batchnorm=False, dropout_rate=dropout_rate, name='recon'+str(i))
+    outputs = conv2d(x=x, num_filters=output_channels, filter_size=(1,1), batchnorm=False,
+                     dropout_rate=dropout_rate, act=False, name='output')
+
+    model = Model(inputs=inputs, outputs=outputs)
+    model.compile(optimizer='adam', loss=img_loss(6+scale))
+    return model, n_filter_list
---
+++ RSSupperRes/utility/IMGfunc.py
@@ -0,0 +1,154 @@
+import numpy as np
+import os
+import imageio
+from PIL import Image
+from keras.utils import Sequence
+
+def pixel_trimmer(image):
+    ''' trim pixel values to the range of 0, 255
+    '''
+    image = np.round(image)
+    return np.clip(image, 0, 255)
+
+
+def get_mse(image1, image2, border_size=0):
+    ''' compute mean square error between two images
+    :param image1, image2: two input images
+    :param border_size: the margin being ignored during the mse computation
+    :return: mean square error
+    :rtype: constant
+    '''
+    if image1.shape[0] != image2.shape[0] or image1.shape[1] != image2.shape[1]:
+        return None
+
+    if len(image1.shape) == 2:
+        image1 = image1.reshape(image1.shape[0], image1.shape[1], 1)
+    if len(image2.shape) == 2:
+        image2 = image2.reshape(image2.shape[0], image2.shape[2], 1)
+
+    image1 = pixel_trimmer(image1)
+    image2 = pixel_trimmer(image2)
+
+    diff = np.subtract(image1, image2)
+    if border_size>0:
+        # trim image into wanted border_size
+        diff = diff[border_size: -border_size, border_size: -border_size]
+
+    return np.mean(np.square(diff))
+
+
+def img_rescale(image, scale, resampling_method):
+    ''' rescale img based on different methods
+    :param image: input image [rows, cols, channels]
+    :param scale: a scale factor from input size to target size
+    :param resampling_method: could be either bicubic, bilinear, or nearest_neighbor
+    :return: a rescaled image based on basic resampling technique
+    :rtype: np.array
+    '''
+    width, height = image.shape[1], image.shape[0]
+    new_width = int(width * scale)
+    new_height = int(height * scale)
+
+    if resampling_method == "bicubic":
+        method = Image.BICUBIC
+    elif resampling_method == "bilinear":
+        method = Image.BILINEAR
+    elif resampling_method == "nearest_neighbor":
+        method = Image.NEAREST
+    else:
+        method = Image.LANCZOS
+
+    if len(image.shape) == 3 and image.shape[2] == 3:
+        image = Image.fromarray(image, "RGB")
+        image = image.resize([new_width, new_height], resample=method)
+        image = np.asarray(image)
+    elif len(image.shape) == 3 and image.shape[2] == 4:
+        # the image may has an alpha channel
+        image = Image.fromarray(image, "RGB")
+        image = image.resize([new_width, new_height], resample=method)
+        image = np.asarray(image)
+    else:
+        image = Image.fromarray(image.reshape(height, width))
+        image = image.resize([new_width, new_height], resample=method)
+        image = np.asarray(image)
+        image = image.reshape(new_height, new_width, 1)
+
+    return image
+
+
+class DataGenerator(Sequence):
+    """ Generates data for RSSuperRes
+    """
+    def __init__(self, img_list, x_dir, y_dir, input_dim, scale, n_channels,
+                 batch_size=32, to_fit=True, shuffle=True):
+        """ Initialization
+        :param img_list: list of image files to use in the generator
+        :param x_dir: path to input images location
+        :param y_dir: path to true images location
+        :param to_fit: True to return X and y, False to return X only
+        :param batch_size: batch size at each iteration
+        :param input_dim: tuple indicating image dimension
+        :param n_channels: number of image channels
+        :param scale: the scale factor maps the input size to output size
+        :param shuffle: True to shuffle label indexes after every epoch
+        """
+        self.img_list = img_list
+        self.x_dir = x_dir
+        self.y_dir = y_dir
+        self.x_dim = np.array(input_dim)
+        self.y_dim = np.array(input_dim) * scale
+        self.scale = scale
+        self.n_channels = n_channels
+        self.to_fit = to_fit
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+        self.on_epoch_end()
+
+    def __len__(self):
+        return int(np.floor(len(self.img_list) / self.batch_size))
+
+    def __getitem__(self, index):
+        # Generate indexes of the batch
+        indexes = self.indexes[index * self.batch_size:(index + 1) * self.batch_size]
+        img_files = [self.img_list[k] for k in indexes]
+
+        # Generate data
+        X = self._generate_X(img_files)
+        if self.to_fit:
+            y = self._generate_y(img_files)
+            return X, y
+        else:
+            return X
+
+    def on_epoch_end(self):
+        """Updates indexes after each epoch
+        """
+        self.indexes = np.arange(len(self.img_list))
+        if self.shuffle == True:
+            np.random.shuffle(self.indexes)
+
+    def _generate_X(self, img_files):
+        """Generates data containing batch_size images
+        :param img_files: list of low resolution images
+        :return: batch of images
+        """
+        X = np.empty((self.batch_size, *self.x_dim, self.n_channels))
+        for i, iFile in enumerate(img_files):
+             tmp_img = imageio.imread(self.x_dir + "/" + iFile).reshape((*self.x_dim, self.n_channels))
+             X[i,] = pixel_trimmer(tmp_img) / 255
+
+        return X
+
+    def _generate_y(self, img_files):
+        """Generates data containing batch_size residual image
+        :param img_files: list of label ids to load
+        :return: batch of residual images
+        """
+        y = np.empty((self.batch_size, *self.y_dim, self.n_channels))
+        for i, iFile in enumerate(img_files):
+            tmp_img_x = imageio.imread(self.x_dir + "/" + iFile).reshape((*self.x_dim, self.n_channels))
+            tmp_img_y = imageio.imread(self.y_dir + "/" + iFile).reshape((*self.y_dim, self.n_channels))
+            tmp_img_x = img_rescale(tmp_img_x, self.scale, 'bicubic')
+            y[i,] = pixel_trimmer(tmp_img_y) - pixel_trimmer(tmp_img_x)
+
+        return y
